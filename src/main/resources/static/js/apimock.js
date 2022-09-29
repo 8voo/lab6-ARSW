a7c4c13aaf78fb99159d36f42c2d050a14d102b9
User <sergioaotero26@hotmail.com>
2022-09-28 23:40:45 -0500

    pinta el canvas

--- src/main/resources/static/js/apimock.js
+++ src/main/resources/static/js/apimock.js
@@ -5,9 +5,11 @@ apimock=(function(){
 	var mockdata=[];
 
 	mockdata["johnconnor"]=	[{author:"johnconnor","points":[{"x":150,"y":120},{"x":215,"y":115}],"name":"house"},
-	 {author:"johnconnor","points":[{"x":340,"y":240},{"x":15,"y":215}],"name":"gear"}];
+	 {author:"johnconnor","points":[{"x":100,"y":100},{"x":15,"y":215}],"name":"gear"}];
 	mockdata["maryweyland"]=[{author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"house2"},
 	 {author:"maryweyland","points":[{"x":140,"y":140},{"x":115,"y":115}],"name":"gear2"}];
+	 mockdata["Enrique"] = [{author:"Enrique","points":[{"x":150,"y":120},{"x":215,"y":115},{"x":110,"y":50}],"name":"3points"},
+	 {author:"Enrique","points":[{"x":10,"y":10},{"x":150,"y":10},{"x":150,"y":150},{"x":10,"y":150},{"x":9,"y":9}],"name":"square"}];
 
     var authorname = $('#autname').val();
     var tamPlanos = [];
@@ -26,13 +28,7 @@ apimock=(function(){
 			callback(
 				mockdata[authname].find(function(e){return e.name===bpname})
 			);
-		},
-
-        changeAuthorName:function(newName, callback){
-            callback(
-                authorname = newName
-            );
-        }
+		}
 	}	
 
 })();
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -15,7 +15,7 @@ var App = (function(){
         var bps = autdata;
         var newPbs = bps.map(function(bp){
             return {
-                name : bp.author,
+                name : bp.name,
                 points : bp.points.length
             }
         });
@@ -25,12 +25,33 @@ var App = (function(){
         bodyTable.html('');
         newPbs.map(function(bp){
             var table = $('#table');
-            
-            table.append("ks<tr> \n <td>"+ bp.name +"</td> \n <td>"+ bp.points +"</td> \n <td><button>Open</button></td> \n </tr>");
+            table.append("<tr> \n <td class = \"bp-name\">"+ bp.name +"</td> \n <td class = \"bp-points\">"+ bp.points +"</td> \n <td><button onclick=App.getUniqueBlueprint(this)>Open</button></td> \n </tr>");
         });
     }
+
+    var getUniqueBlueprint = function(bp){
+        var row = bp.parentElement.parentElement;
+        var autname = $("#autname").val();
+        var bpName = row.firstElementChild.innerText;
+        apimock.getBlueprintsByNameAndAuthor(autname, bpName ,paintPoints);
+    }
     
+    var paintPoints = function(bpData){
+        var canva = document.querySelector('canvas');
+        var points = bpData.points;
+        var quanPoints = points.length
+        for (var i = 0 ; i < quanPoints-1; i++){
+            ctx = canva.getContext("2d");
+            ctx.moveTo(points[i].x,points[i].y);
+            ctx.lineTo(points[i+1].x,points[i+1].y);
+            ctx.clearRect(0, 0, canva.width, canva.height);
+            ctx.stroke();
+        }
+        
+    }
+
     return{
+        getUniqueBlueprint : getUniqueBlueprint,
         getBlueprints : getBlueprints
     }
 })();
